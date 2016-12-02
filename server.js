4987261769136e7e759e4636bde944d660990939
snowgoing <snowgoing@gmail.com>
2016-12-01 19:18:34 -0800

    Fix Page Title Variable

--- server.js
+++ server.js
@@ -55,7 +55,9 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/projects', (req, res) => {
-  res.render('projects.hbs');
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page'
+  });
 });
 
 app.get('/bad', (req, res) => {
